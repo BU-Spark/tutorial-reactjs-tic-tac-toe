9e4f7e57a8037b01764317a5ba406b0baffbb585
Ian Saucy <iansaucy@gmail.com>
2023-09-26 14:22:59 -0400

    ica0

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "../styles/global.css";
+import type { AppProps } from "next/app";
+// Don't edit this file
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
